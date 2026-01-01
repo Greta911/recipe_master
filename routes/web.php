d432a9462ea64b2b4365ac0356be2e66abdf2ff5
Greta911 <grema20012000@yahoo.it>
2026-01-01 14:56:27 +0100

    Installation Tailwind 4 en locale

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 //ROUTE PAR DEFAUT
 //PATTERN: /
-//VUE: welcome.blade.php
+//VUE: templates/index.blade.php
 Route::get('/', function () {
-    return view('welcome');
+    return view('template.index');
 })->name('home');
-
-//ROUTE CATEGORIES
-//PATTERN: /categories
-//VUE: index.blade.php
-Route::get('/categories', function () {
-    return view('categories.index');
-})->name('categories');
-
-//ROUTE INGREDIENTS
-//PATTERN: /ingredients
-//VUE: index.blade.php
-Route::get('/ingredients', function () {
-    return view('ingredients.index');
-})->name('ingredients');
-
-//ROUTE RECETTES
-//PATTERN: /recipes
-//VUE: index.blade.php
-Route::get('/recipes', function () {
-    return view('recipes.index');
-})->name('recipes.index');
